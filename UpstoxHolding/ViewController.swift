5a8299590a304ba6836aec366454b0dc07393739
narpat22 <pritesh730@gmail.com>
2024-11-12 16:14:48 +0530

    Initial Commit

---
+++ UpstoxHolding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpstoxHolding
+//
+//  Created by Pritesh Singhvi on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
